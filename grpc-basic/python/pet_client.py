d016b726d06ff27249c71853130a72d5753a5111
Christoffer Cortes Sjöwall <christoffer.cortes.sjowall@ericsson.com>
2021-10-12 10:29:28 +0200

    Hej hej

--- grpc-basic/python/pet_client.py
+++ grpc-basic/python/pet_client.py
@@ -6,8 +6,13 @@ from interfaces import pet_pb2_grpc
 def run():
     with grpc.insecure_channel('localhost:50053') as channel:
         stub = pet_pb2_grpc.PetStub(channel)
-        sys.argv[1]
-        response = stub.GetPet(pet_pb2.PetRequest(size=pet_pb2.PetRequest.Size.LARGE))
+        pet_size = "LARGE"
+        try:
+            pet_size = pet_pb2.PetRequest.Size.Value(sys.argv[1])
+        except Exception:
+            print(f"Received pet size '{sys.argv[1]}' is not a valid size")
+
+        response = stub.GetPet(pet_pb2.PetRequest(size=pet_size))
         print(response)
 
 if __name__ == '__main__':
