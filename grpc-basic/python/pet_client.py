a88f1b82a1edda1ddc5432f67658590d66977071
Christoffer Cortes Sjöwall <christoffer.cortes.sjowall@ericsson.com>
2021-10-14 09:22:57 +0200

    Lite saker

--- grpc-basic/python/pet_client.py
+++ grpc-basic/python/pet_client.py
@@ -6,6 +6,9 @@ from interfaces import pet_pb2_grpc
 def run():
     with grpc.insecure_channel('localhost:50053') as channel:
         stub = pet_pb2_grpc.PetStub(channel)
+        if len(sys.argv) < 2:
+            print(f"Must provide pet size {pet_pb2.PetRequest.Size}")
+            return
         pet_size = "FOO"
         try:
             pet_size = pet_pb2.PetRequest.Size.Value(sys.argv[1])
@@ -16,10 +19,7 @@ def run():
             response = stub.GetPet(pet_pb2.PetRequest(size=pet_size))
             print(response)
         except grpc.RpcError as e:
-            print(e.details())
-            status_code = e.code()
-            print(status_code.name)
-            print(status_code.value)
+            print(f"ERROR: Trying to get pet - {e.details()}")
 
 if __name__ == '__main__':
     run()
