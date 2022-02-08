117a9c3458014868ef8fab6090c5be749a51641f
Christoffer Cortes Sjöwall <christoffer.cortes.sjowall@ericsson.com>
2022-02-08 10:15:30 +0100

    pet-shop app creation!

---
+++ pet-shop/src/app/pets/pets.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Pet } from '../pet';
+
+@Component({
+  selector: 'app-pets',
+  templateUrl: './pets.component.html',
+  styleUrls: ['./pets.component.css']
+})
+export class PetsComponent implements OnInit {
+
+  pet: Pet = {
+    id: 1,
+    name: "Pricken"
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
