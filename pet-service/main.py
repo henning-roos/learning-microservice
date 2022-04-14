88fb305f7b076f8aabfe5e693df3b359d9c988b0
Henning Roos <henning.roos@ericsson.com>
2022-04-14 13:03:46 +0200

    Flask support

---
+++ pet-service/main.py
@@ -0,0 +1,17 @@
+import logging
+from flask import Flask, redirect
+
+LOGGER = logging.getLogger("main")
+
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return redirect("/pets/")
+
+@app.route('/pets/', methods=['GET', 'POST'])
+def pets():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    app.run(host='0.0.0.0', port=105)
